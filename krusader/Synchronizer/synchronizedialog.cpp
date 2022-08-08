156064b266e0f294a66eb8b18a4e727bde5b2b1b
Toni Asensi Esteve <toni.asensi@kdemail.net>
2022-08-08 23:58:11 +0200

    Start synchronizing when Return is pressed in the last step before the synchronization
    This change makes sure that the synchronizing starts when its final dialog is shown
    and Return is pressed, as it used to be when using e.g. Kubuntu 20.04.
    
    Revision: https://invent.kde.org/utilities/krusader/-/merge_requests/94

--- krusader/Synchronizer/synchronizedialog.cpp
+++ krusader/Synchronizer/synchronizedialog.cpp
@@ -16,6 +16,7 @@
 #include <QVBoxLayout>
 #include <QLabel>
 
+
 #include <KConfigCore/KSharedConfig>
 #include <KI18n/KLocalizedString>
 
@@ -98,6 +99,7 @@ SynchronizeDialog::SynchronizeDialog(QWidget* parent,
     btnStart = new QPushButton(hboxWidget);
     btnStart->setText(i18n("&Start"));
     btnStart->setIcon(Icon("media-playback-start"));
+    btnStart->setDefault(true);
     hbox->addWidget(btnStart);
 
     btnPause = new QPushButton(hboxWidget);
