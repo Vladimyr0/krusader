73f3ac7d571e64661b1c28de631f15b63911c144
Paul Korecky <paul.c.korecky@outlook.com>
2025-12-24 15:02:06 +0100

    simplify per review

--- app/krusaderview.cpp
+++ app/krusaderview.cpp
@@ -189,10 +189,8 @@ PanelManager *KrusaderView::createManager(bool left)
 void KrusaderView::updateCurrentActivePath()
 {
     const QString path = activePanel()->gui->lastLocalPath();
-    QFileInfo fi(path);
-
     _cmdLine->setCurrent(path);
-    emit activePathChanged(fi.fileName());
+    emit activePathChanged(QFileInfo(path).fileName());
     KConfigGroup cfg = krConfig->group("General");
     if (_terminalDock->isInitialised() && cfg.readEntry("Send CDs", _SendCDs)) {
         _terminalDock->sendCd(path);
