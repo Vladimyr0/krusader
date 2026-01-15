f109a8cb0be4ed230fe2e10605874cabb6f96476
Vladimyr0 <vladimyr0@yandex.ru>
2026-01-16 01:10:57 +0300

    поменял местами кнопки F2 и F9 внизу окна

--- app/GUI/kfnkeys.cpp
+++ app/GUI/kfnkeys.cpp
@@ -22,10 +22,10 @@ KFnKeys::KFnKeys(QWidget *parent, KrMainWindow *mainWindow)
     : QWidget(parent)
     , mainWindow(mainWindow)
 {
-    buttonList << setup(mainWindow->listPanelActions()->actRenameF2, i18n("Rename")) << setup(mainWindow->listPanelActions()->actViewFileF3, i18n("View"))
+    buttonList << setup(mainWindow->listPanelActions()->actTerminalF9, i18n("Term")) << setup(mainWindow->listPanelActions()->actViewFileF3, i18n("View"))
                << setup(mainWindow->listPanelActions()->actEditFileF4, i18n("Edit")) << setup(mainWindow->listPanelActions()->actCopyF5, i18n("Copy"))
                << setup(mainWindow->listPanelActions()->actMoveF6, i18n("Move")) << setup(mainWindow->listPanelActions()->actNewFolderF7, i18n("Mkdir"))
-               << setup(mainWindow->listPanelActions()->actDeleteF8, i18n("Delete")) << setup(mainWindow->listPanelActions()->actTerminalF9, i18n("Term"))
+               << setup(mainWindow->listPanelActions()->actDeleteF8, i18n("Delete")) << setup(mainWindow->listPanelActions()->actRenameF2, i18n("Rename"))
                << setup(mainWindow->krActions()->actF10Quit, i18n("Quit"));
 
     updateShortcuts();
